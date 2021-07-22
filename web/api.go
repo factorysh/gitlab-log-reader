5a99be432228e902d06085b56c9a3d87901d301f
Wilfried OLLIVIER <wollivier@bearstech.com>
2021-07-22 14:03:58 +0200

    Add logging fields

--- web/api.go
+++ web/api.go
@@ -1,7 +1,6 @@
 package web
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/apex/log"
@@ -19,19 +18,26 @@ func NewAPI(_rg *rg.RG) *API {
 }
 
 func (a *API) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	p := r.Header.Get("x-project")
+	rip := r.Header.Get("x-remote")
 	w.Header().Set("Server", "Log-Reader")
-	fmt.Println("headers", r.Header)
 	if r.Method != "GET" {
+		log.WithFields(log.Fields{
+			"remote ip": rip,
+			"method":    r.Method}).Info("Received invalid method")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	p := r.Header.Get("x-project")
-	rip := r.Header.Get("x-remote")
 	code := http.StatusForbidden
 	if a.rg.Exists(p, rip) {
 		code = http.StatusOK
 	}
 	w.WriteHeader(code)
-	log.WithFields(log.Fields{"project": p, "remote ip": rip})
+	log.WithFields(log.Fields{
+		"project":                  p,
+		"remote ip":                rip,
+		"status code":              code,
+		"auth request remote addr": r.RemoteAddr,
+		"auth request headers":     r.Header}).Info("Sending response")
 }
